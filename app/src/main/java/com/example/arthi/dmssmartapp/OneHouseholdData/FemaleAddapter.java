603715fafc7be414a688c6bf2d0befeab3d515c9
gopalsa <gopal.ecretail@gmail.com>
2019-08-22 12:37:44 +0530

    22/8/19

--- app/src/main/java/com/example/arthi/dmssmartapp/OneHouseholdData/FemaleAddapter.java
+++ app/src/main/java/com/example/arthi/dmssmartapp/OneHouseholdData/FemaleAddapter.java
@@ -2,6 +2,7 @@ package com.example.arthi.dmssmartapp.OneHouseholdData;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
